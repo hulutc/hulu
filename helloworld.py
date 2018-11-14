63051179c1678cf09aeadfab82d91566ccf3b36a
hulutc <w50253082@gmail.com>
2018-11-14 09:20:49 +0800

    update

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,20 @@
-if __name__ == '__main__':
-    for i in range(20):
-        print("tiancheng")
-    print("done")
+from flask import Flask
+# from redis import Redis
+import os
+
+app = Flask(__name__)
+# redis = Redis(host='redis', port=6379)
+
+COUNT = 1
+
+
+@app.route('/')
+def hello():
+    # redis.incr('hits')
+    global COUNT
+    COUNT += 1
+    return f'Hello World! I have been seen {COUNT} times.'
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", debug=True, port=3455)
